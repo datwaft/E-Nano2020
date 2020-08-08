742a800ea5a166a1340785657af5e79fe0834e3f
datwaft <datwaftx@gmail.com>
2020-08-07 18:35:59 -0600

    Fixed colors & fixed not found petitions

--- E-Nano2020/src/main/java/com/group03/App.java
+++ E-Nano2020/src/main/java/com/group03/App.java
@@ -4,23 +4,30 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.regex.Pattern;
 
+import com.diogonunes.jcolor.AnsiFormat;
+import com.diogonunes.jcolor.Attribute;
+
 import fi.iki.elonen.NanoHTTPD;
 import fi.iki.elonen.NanoHTTPD.Response.Status;
   
 public class App extends NanoHTTPD {
-  Pattern isFile = Pattern.compile("^.*[^/]$");
+  private static Pattern isFile = Pattern.compile("^.*[^/]$");
+  private static AnsiFormat fError = new AnsiFormat(Attribute.RED_TEXT(), Attribute.BOLD());
+  private static AnsiFormat fWarning = new AnsiFormat(Attribute.YELLOW_TEXT(), Attribute.BOLD());
+  private static AnsiFormat fSuccess = new AnsiFormat(Attribute.GREEN_TEXT());
+  private static final Integer port = 8080;
 
   public App() throws IOException {
-    super(8080);
-    start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
-    System.out.println("\n\u001B[32mThe webserver is running.\u001B[0m\nThe url is http://localhost:8080/\n");
+    super(port);
+    start(SOCKET_READ_TIMEOUT, false);
+    System.out.println("\n" + fSuccess.format("The webserver is running on the port " + port + ".") + "\n" + "The url is http://localhost:" + port + "/" + "\n");
   }
 
   public static void main(String[] args) {
     try {
       new App();
     } catch (IOException ioe) {
-      System.err.println("\u001B[31mCouldn't start server:\u001B[0m\n" + ioe);
+      System.err.println("\n" + fError.format("Couldn't start server:" + ioe) + "\n");
     }
   }
 
@@ -30,9 +37,11 @@ public class App extends NanoHTTPD {
     String file = (isFile.matcher(uri).matches() ? uri : uri + "index.html");
     InputStream fileStream = getClass().getClassLoader().getResourceAsStream(file.substring(1));
     if (fileStream != null) {
-      return NanoHTTPD.newChunkedResponse(Status.OK, getMimeTypeForFile(file), fileStream);
+      System.out.println(fSuccess.format("Succesful response to '" + file + "' petition."));
+      return newChunkedResponse(Status.OK, getMimeTypeForFile(file), fileStream);
     } else {
-      return newFixedLengthResponse("<html><body><h1>Error 404: File '" + file + "' not found</h1></body></html>");
+      System.err.println(fWarning.format("Resource not found: '" + file + "'."));
+      return newFixedLengthResponse(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "The requested resource does not exist");
     }
   }
 }
