a15d45324f8d254a101333e60da8919a24ed2409
datwaft <datwaftx@gmail.com>
2020-08-27 18:54:05 -0600

    Changed to router

--- E-Nano2020/src/main/java/com/group03/App.java
+++ E-Nano2020/src/main/java/com/group03/App.java
@@ -2,15 +2,16 @@ package com.group03;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 import com.diogonunes.jcolor.AnsiFormat;
 import com.diogonunes.jcolor.Attribute;
 
-import fi.iki.elonen.NanoHTTPD;
+import fi.iki.elonen.router.RouterNanoHTTPD;
 import fi.iki.elonen.NanoHTTPD.Response.Status;
   
-public class App extends NanoHTTPD {
+public class App extends RouterNanoHTTPD {
   private static Pattern isFile = Pattern.compile("^.*[^/]$");
   private static AnsiFormat fError = new AnsiFormat(Attribute.RED_TEXT(), Attribute.BOLD());
   private static AnsiFormat fWarning = new AnsiFormat(Attribute.YELLOW_TEXT(), Attribute.BOLD());
@@ -19,7 +20,8 @@ public class App extends NanoHTTPD {
 
   public App() throws IOException {
     super(port);
-    start(SOCKET_READ_TIMEOUT, false);
+    this.addMappings();
+    this.start(SOCKET_READ_TIMEOUT, false);
     System.out.println("\n" + fSuccess.format("The webserver is running on the port " + port + ".") + "\n" + "The url is http://localhost:" + port + "/" + "\n");
   }
 
@@ -32,16 +34,23 @@ public class App extends NanoHTTPD {
   }
 
   @Override
-  public Response serve(IHTTPSession session) {
-    String uri = session.getUri();
-    String file = (isFile.matcher(uri).matches() ? uri : uri + "index.html");
-    InputStream fileStream = getClass().getClassLoader().getResourceAsStream(file.substring(1));
-    if (fileStream != null) {
-      System.out.println(fSuccess.format("Succesful response to '" + file + "' petition [" + session.getMethod() + "]."));
-      return newChunkedResponse(Status.OK, getMimeTypeForFile(file), fileStream);
-    } else {
-      System.err.println(fWarning.format("Resource not found: '" + file + "' [" + session.getMethod() + "]."));
-      return newFixedLengthResponse(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "The requested resource does not exist");
+  public void addMappings() {
+    this.addRoute("/(.)*", ResourceHandler.class);
+  }
+
+  public static class ResourceHandler extends StaticPageHandler {
+    @Override
+    public Response get(UriResource uriResource, Map<String, String> urlParams, IHTTPSession session) {
+      String uri = session.getUri();
+      String file = (isFile.matcher(uri).matches() ? uri : uri + "index.html");
+      InputStream fileStream = getClass().getClassLoader().getResourceAsStream(file.substring(1));
+      if (fileStream != null) {
+        System.out.println(fSuccess.format("Succesful response to '" + file + "' petition [" + session.getMethod() + "]."));
+        return newChunkedResponse(getStatus(), getMimeTypeForFile(file), fileStream);
+      } else {
+        System.err.println(fWarning.format("Resource not found: '" + file + "' [" + session.getMethod() + "]."));
+        return newFixedLengthResponse(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "The requested resource does not exist");
+      }
     }
   }
 }
