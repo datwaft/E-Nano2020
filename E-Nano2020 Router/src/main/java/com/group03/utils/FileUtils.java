a50e969ff4f62d9795ada5833e3dc4de0abd411d
David Guevara <datwaftx@gmail.com>
2020-09-30 16:44:42 -0600

    Port is extracted from filesystem
    File is server.properties

---
+++ E-Nano2020 Router/src/main/java/com/group03/utils/FileUtils.java
@@ -0,0 +1,52 @@
+/**
+ *  _____      _   _                  ____   ___ ____   ___
+ * | ____|    | \ | | __ _ _ __   ___|___ \ / _ \___ \ / _ \
+ * |  _| _____|  \| |/ _` | '_ \ / _ \ __) | | | |__) | | | |
+ * | |__|_____| |\  | (_| | | | | (_) / __/| |_| / __/| |_| |
+ * |_____|    |_| \_|\__,_|_| |_|\___/_____|\___/_____|\___/
+ *
+ * File: FileUtils.java
+ * Description:
+ *    Archivo de ayuda para leer archivos del filesystem.
+ * Authors:
+ * - David Alberto Guevara Sánchez
+ *   402450355
+ * - Joy Bonilla Fley
+ *   402360421
+ * - Jose Barrantes Araya
+ *   207600954
+ * - Natalia Solano Azofeifa
+ *   117290958
+ * - Luis David Villalobos Gonzalez
+ *   117540697
+ * Group: 03
+ * Schedule: 10am
+ * Date of modification: 2020-09-30
+ */
+
+package com.group03.utils;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+public class FileUtils {
+
+  private FileUtils() { }
+
+  public static Map<String, String> readProperties(String filename) {
+    Properties properties = new Properties();
+
+    try (var input_stream = FileUtils.class.getClassLoader().getResourceAsStream(filename)) {
+      properties.load(input_stream);
+
+      return properties.entrySet().stream().collect(
+        Collectors.toMap(e -> e.getKey().toString(), e -> e.getValue().toString())
+      );
+    } catch(Exception ex) {
+      OutUtils.errorFormatWithDatetime("File %s failed to load.", filename);
+      return null;
+    }
+  }
+
+}
