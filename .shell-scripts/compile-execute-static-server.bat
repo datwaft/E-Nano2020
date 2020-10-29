d10e49ea90c3c19d5bfdb6df6ba7ab226ae17ecc
David Guevara <datwaftx@gmail.com>
2020-10-29 01:57:37 -0600

    Fixed scripts for Gradle

--- .shell-scripts/compile-execute-static-server.bat
+++ .shell-scripts/compile-execute-static-server.bat
@@ -4,9 +4,9 @@
 :: | |__|_____| |\  | (_| | | | | (_) / __/| |_| / __/| |_| |
 :: |_____|    |_| \_|\__,_|_| |_|\___/_____|\___/_____|\___/
 ::
-:: File: compile-execute.java
+:: File: compile-execute-static-server.java
 :: Description:
-::    Script para compilar y ejecutar todos los servidores
+::    Script para compilar y ejecutar el servidor de archivos estáticos.
 :: Authors:
 :: - David Alberto Guevara Sánchez
 ::   402450355
@@ -20,16 +20,31 @@
 ::   117540697
 :: Group: 03
 :: Schedule: 10am
-:: Date of modification: 2020-10-17
+:: Date of modification: 2020-10-29
 
 @echo off
 setlocal
+title Static Server
 
-echo Calling router compiler and executer...
-start .\.shell-scripts\compile-execute-router.bat
-echo Calling static server compiler and executer...
-start .\.shell-scripts\compile-execute-static-server.bat
-echo Calling prolog server executer...
-start .\.shell-scripts\compile-execute-transpiler.bat
+echo Moving to folder...
+cd "static-server"
+
+echo.
+echo Compiling...
+call gradlew build
+if errorlevel 1 goto :error
+
+echo.
+echo Executing...
+call gradlew run
+goto :end
+
+:error
+
+echo.
+echo The compilation has errors, cannot run.
+
+:end
 
 exit /b %errorlevel%
+
