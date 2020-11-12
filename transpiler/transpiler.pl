8f815ceea2bbfc745cb10c5c5f123ce0477c2c3c
David Guevara <datwaftx@gmail.com>
2020-11-12 12:29:43 -0600

    Term added

--- transpiler/transpiler.pl
+++ transpiler/transpiler.pl
@@ -12,7 +12,19 @@
 
 % Assignment definition
 % =====================
-  assignment(assignment(Name, _)) --> variable_name(Name), " = ", "value", ";".
+  assignment(assignment(Name, _)) --> variable_name(Name), " = ", "value".
+% Term
+% ====
+  term(number(Term)) --> [Term].
+  term(string(Term)) --> [Term].
+  term(list(Term)) --> "List.of(", list_body(Term), ")".
+  term(const(Term)) --> [Term].
+  term(Term) --> variable_name(Term).
+  % List body
+  list_body([]) --> [].
+  list_body([Term]) --> term(Term), {!}.
+  list_body([Term | Rest]) --> term(Term), ", ", list_body(Rest).
+  % TODO: change term(Term) for general_body(Term).
 % Identifier definition
 % =====================
   % Variable name
