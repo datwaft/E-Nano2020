54ef755c9f74c2fb72ec6d68a6cf845e94d80281
David Guevara <datwaftx@gmail.com>
2020-11-15 22:57:39 -0600

    Comentarios en código prolog

--- transpiler/parser.pl
+++ transpiler/parser.pl
@@ -1,3 +1,29 @@
+/*
+ *  _____      _   _                  ____   ___ ____   ___
+ * | ____|    | \ | | __ _ _ __   ___|___ \ / _ \___ \ / _ \
+ * |  _| _____|  \| |/ _` | '_ \ / _ \ __) | | | |__) | | | |
+ * | |__|_____| |\  | (_| | | | | (_) / __/| |_| / __/| |_| |
+ * |_____|    |_| \_|\__,_|_| |_|\___/_____|\___/_____|\___/
+ *
+ * File: parser.pl
+ * Description:
+ *    Contiene las utilidades de prolog para parsear un archivo de expresión a partir de tokens.
+ * Authors:
+ * - David Alberto Guevara Sánchez
+ *   402450355
+ * - Joy Bonilla Fley
+ *   402360421
+ * - Jose Barrantes Araya
+ *   207600954
+ * - Natalia Solano Azofeifa
+ *   117290958
+ * - Luis David Villalobos Gonzalez
+ *   117540697
+ * Group: 03
+ * Schedule: 10am
+ * Date of modification: 2020-11-15
+ */
+
 :- module(parser, [
   program/3
 ]).
